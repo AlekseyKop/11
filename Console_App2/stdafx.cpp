226924858bf6bda82ad8717dd9e94e2bc52ebb42
AlekseyKop <32506099+AlekseyKop@users.noreply.github.com>
2017-10-25 00:45:43 +0300

    Add files via upload

---
+++ Console_App2/stdafx.cpp
@@ -0,0 +1,13 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Console_App2.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <locale.h>
+#include <sstream>
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
